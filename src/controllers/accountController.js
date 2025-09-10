95d287db25e02084847dd1093a8e8c2dc997e10b
LPGP Connect Dev Profile <150681710+lpgp-connect-dev@users.noreply.github.com>
2025-09-10 14:19:42 +0800

    feat: implement account creation and login functionality with validation

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -1,16 +1,87 @@
 import { Account } from "../models/index.js";
 
-const createAccount = async (req, res) => {
+import isValidEmail from "../utils/validEmail.js";
+
+const createAccount = async (request, response) => {
 	try {
-		const { email, username,  password } = req.body;
+		const { email, username, password } = request.body;
+
+		if (!email || !username || !password) {
+			return response.status(400).json({
+				status: "error",
+				message: "Email, username, and password are required",
+			});
+		}
+
+		const existingEmail = await Account.findOne({ email });
+		if (existingEmail) {
+			return response.status(400).json({
+				status: "error",
+				message: "Email is already taken",
+			});
+		}
+		if (!isValidEmail(email)) {
+			return response.status(400).json({
+				status: "error",
+				message: "Invalid email format",
+			});
+		}
+		const existingUsername = await Account.findOne({ username });
+		if (existingUsername) {
+			return response.status(400).json({
+				status: "error",
+				message: "Username is already taken",
+			});
+		}
+
 		const newAccount = new Account({ username, email, password });
 		await newAccount.save();
-		res.status(201).json({ message: "Account created successfully" });
+		return response.status(201).json({
+			status: "success",
+			message: "Account created successfully",
+		});
 	} catch (error) {
-		res.status(500).json({ message: "Failed to create account" });
+		return response.status(500).json({
+			status: "error",
+			message: "Failed to create account",
+		});
+	}
+};
+
+const loginAccount = async (request, response) => {
+	try {
+		const { username, password } = request.body;
+
+		if (!username || !password) {
+			return response
+				.status(400)
+				.json({
+					status: "error",
+					message: "Username and password are required",
+				});
+		}
+
+		const account = await Account.findOne({ username, password });
+
+		if (!account) {
+			return response
+                .status(401)
+                .json({
+                    status: "error",
+                    message: "Invalid username or password",
+                });
+		} 
+
+        return response.status(200).json({
+            status: "success",
+            message: "Login successful",
+        });
+	} catch (error) {
+		res.status(500).json({ status: "error", message: "Failed to login" });
 	}
 };
 
 export default {
 	createAccount,
+	loginAccount,
 };
--- src/routes/accountRoutes.js
+++ src/routes/accountRoutes.js
@@ -5,5 +5,6 @@ import accountController from "../controllers/accountController.js";
 const router = Router();
 
 router.post("/create-account", accountController.createAccount);
+router.post("/login", accountController.loginAccount);
 
 export default router;
